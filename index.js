a3d4e9b197f5fa7e23ce719cc8da8d79709f61d9
Muneeb Khalid <106615218+SYEDMUNEEEB@users.noreply.github.com>
2024-06-25 13:32:11 +0500

    now

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-// netlify/functions/server.js
+// backend/index.js
 
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path'); // Import the path package
-const Result = require('./models/results'); // Import your model
+const path = require('path');
 const serverless = require('serverless-http');
+const Result = require('./models/results'); // Adjust the path to your model
 
 const app = express();
-const port = process.env.PORT || 4000; // Port for your server
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://result:result@cluster0.ajwm45j.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://result:result@cluster0.ajwm45j.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: 'https://ieltsidp.online/' })); // Enable CORS for your frontend
+app.use(cors({ origin: 'https://ieltsidp.online/' }));
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/results/build')));
+app.use(express.static(path.join(__dirname, 'frontend_build')));
 
 // API Routes
 app.post('/results', async (req, res) => {
@@ -49,12 +48,12 @@ app.get('/results/:passport', async (req, res) => {
 });
 
 app.get("/", async (req, res) => {
-  res.send("hello from backend")
+  res.send("hello from backend");
 });
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/results/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend_build', 'index.html'));
 });
 
 module.exports.handler = serverless(app);
