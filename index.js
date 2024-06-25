fff95fb77b4cad12ca6b2bf30dfbff9da295fa2b
Muneeb Khalid <106615218+SYEDMUNEEEB@users.noreply.github.com>
2024-06-25 13:52:23 +0500

    new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const serverless = require('serverless-http');
+
 const Result = require('./models/results'); // Adjust the path to your model
 
 const app = express();
@@ -55,5 +55,7 @@ app.get("/", async (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend_build', 'index.html'));
 });
-
-module.exports.handler = serverless(app);
+app.listen(
+  process.env.PORT || 5000,
+  () => console.log(`Listening on port ${process.env.PORT || 5000}!`)
+)
